d28c858cb3ef305726a06dda09f3e2d29cf1eca1
mohamed_bouth <mohamedbouth87@gmail.com>
2026-09-01 15:51:03 +0100

    exercice 03 :  Le compte à rebours de lancement

---
+++ exercice-03.js
@@ -0,0 +1,58 @@
+// //1
+
+// for (i = 10 ; i > 0 ; i--){
+//     console.log(i)
+//     if(i === 1){
+//         console.log('Décollage !')
+//     }
+// }
+
+// //2
+
+// let somme = 0;
+// for(i = 1 ; i <= 100 ; i++){
+//     somme += i;
+// }
+// console.log(somme)
+
+// //3
+
+// for(i = 1 ; i <= 20 ; i++){
+//     if( i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+//4.1
+
+// let i = 10
+// while(i > 0){
+//     console.log(i)
+//     if(i === 1){
+//         console.log('Décollage ! \n')
+//     }
+//     i--
+// }
+
+//4.2
+
+// let i = 1;
+// let somme = 0;
+
+// while (i <= 100) {
+//     somme += i;
+//     i++
+// }
+// console.log(somme)
+
+//4.3
+
+// let i = 1;
+
+// while(i <= 20){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
