1f6a0a099d86ee116c3107b3556c8c35187130df
Laraibi Mehdi <meh.laraibi@gmail.com>
2026-09-02 20:32:30 +0100

    fix: declare sales report array variable

--- exercice-10.js
+++ exercice-10.js
@@ -52,7 +52,7 @@ function genererRapport(ventes) {
 Chiffre d'affaires total : ${totalMontant} DH\n
 Meilleure vente : ${biggestSell.produit} (${biggestSell.vendeur}) - ${biggestSell.montant} DH\n
 CA par vendeur :\n`
-    caParVendeurArray = Object.entries(caParVendeur)
+    const caParVendeurArray = Object.entries(caParVendeur)
     console.log(caParVendeurArray)
     for (let i = 0; i < caParVendeurArray.length; i++) {
         text += `${caParVendeurArray[i][0]} : ${caParVendeurArray[i][1]} DH \n`;
